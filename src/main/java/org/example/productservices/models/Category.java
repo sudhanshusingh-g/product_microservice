0bb2988a87e5cfef730d3082ef78f799800c657c
sudhanshusingh-g <sudsingh32@gmail.com>
2024-12-19 21:40:21 +0530

    DB connection

--- src/main/java/org/example/productservices/models/Category.java
+++ src/main/java/org/example/productservices/models/Category.java
@@ -1,11 +1,18 @@
 package org.example.productservices.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String title;
 }
--- src/main/java/org/example/productservices/models/Product.java
+++ src/main/java/org/example/productservices/models/Product.java
@@ -1,15 +1,20 @@
 package org.example.productservices.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String description;
     private double price;
+    @ManyToOne
     private Category category;
     private String image;
 }
